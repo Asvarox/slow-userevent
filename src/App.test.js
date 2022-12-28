7f934cbb6aeb40cb91a92df2b889adb4e7a85782
Aleksander Tatarczyk <atatarczyk@ecovadisazure.onmicrosoft.com>
2022-12-28 08:54:10 +0100

    user event 14 test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test.each([['a'], ['text'], ['a bit longer text']])('Input "%p"', async (text) => {
+  const user = userEvent.setup();
+  render(<input data-testid="input" type="text" />);
+  const input = screen.getByTestId('input');
+
+  console.time(text);
+  await user.type(input, text)
+  console.timeEnd(text);
 });
